83b22b58b5fd1e6efcb8a913e12c78d5f1164940
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-01-27 18:10:23 +0100

    Editing posts

--- web_applications/blog/blog/urls.py
+++ web_applications/blog/blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', view=views.index, name='index'),
     path('blogs/<int:blog_id>', view=views.blog, name='blog'),
     path('posts/<int:post_id>', view=views.post, name='post'),
+    path('make_blog', view=views.make_blog, name='make_blog'),
+    path('make_post/<int:blog_id>', view=views.make_post, name='make_post'),
+    path('edit_post/<int:post_id>', view=views.edit_post, name='edit_post'),
 ]
--- web_applications/blog/blog/views.py
+++ web_applications/blog/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog, BlogPost
+from .forms import BlogForm, BlogPostForm
 
 # Create your views here.
 def index(request):
@@ -21,6 +22,64 @@ def blog(request, blog_id):
 def post(request, post_id):
     post = BlogPost.objects.get(id=post_id)
     context = {
+        'blog': post.blog,
         'post': post
     }
     return render(request, 'blog/post.html', context=context)
+
+def make_blog(request):
+    if request.method != 'POST':
+        form = BlogForm()
+
+    else:
+        data = request.POST
+        form = BlogForm(data)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:index')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/make_blog.html', context=context)
+
+
+def make_post(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    if request.method != 'POST':
+        form = BlogPostForm()
+
+    else:
+        data = request.POST
+        form = BlogPostForm(data)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.blog = blog
+            post.save()
+            return redirect('blog:blog', blog.id)
+
+    context = {
+        'form': form,
+        'blog': blog
+    }
+    return render(request, 'blog/make_post.html', context=context)
+
+def edit_post(request, post_id):
+    post = BlogPost.objects.get(id=post_id)
+    blog = post.blog
+    if request.method != 'POST':
+        form = BlogPostForm(instance=post)
+
+    else:
+        data = request.POST
+        form = BlogPostForm(instance=post, data=data)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:post', post.id)
+
+    context = {
+        'form': form,
+        'post': post,
+        'blog': blog,
+    }
+    return render(request, 'blog/edit_post.html', context=context)
