be4aeff9d21d392b1287b48f1a4c56b2cb008cb7
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-01-27 23:25:12 +0100

    Login and registration

--- web_applications/blog/blog_project/settings.py
+++ web_applications/blog/blog_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog',
+    'accounts',
     # Default
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'blog:index'
+LOGOUT_REDIRECT_URL = 'blog:index'
